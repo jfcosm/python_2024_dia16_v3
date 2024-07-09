232bdc4b0c238b1a083f1963eb7a41e55feaf092
Jose Francisco Carle Carrera <jfco-soho@MacBook-Air-de-Jose.local>
2024-07-09 17:38:41 -0600

    Agregado el año 2029 en el footer para no tener que actualizarlo por 5 años

---
+++ Assets/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('[data-bs-toggle="tooltip"]').tooltip();
+});
+
+
+
+$(document).ready(function() {
+    $('#enviarCorreo').tooltip({
+        title: 'Enviar por correo',
+        placement: 'top'
+    });
+});
+
+$(document).ready(function() {
+    $('#anadiraFavoritos').tooltip({
+        title: 'Añadir a Favoritos',
+        placement: 'top'
+    });
+});
